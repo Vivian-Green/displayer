50f58b671d9e0d33a40dd3275dc0e362c993b4a3
Vivian Corbett <acorbett0528@gmail.com>
2024-02-21 08:42:08 -0800

    clean up EventListeners

--- src/main/java/me/vivian/displayer/EventListeners.java
+++ src/main/java/me/vivian/displayer/EventListeners.java
@@ -15,6 +15,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.joml.Vector2i;
 import org.joml.Vector3d;
@@ -70,8 +71,6 @@ public final class EventListeners extends JavaPlugin implements Listener {
      */
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        System.out.println("click within an inventory");
-        // ensure this is the display gui
         if (!event.getView().getTitle().equals("display GUI")) {
             return;
         }
@@ -79,113 +78,106 @@ public final class EventListeners extends JavaPlugin implements Listener {
         event.setCancelled(true);
         Player player = (Player) event.getWhoClicked();
 
-        int slot = event.getRawSlot(); // Get the raw slot number
+        int slot = event.getRawSlot();
 
-        // ensure the clicked slot is within the inventory gui (not player inv)
+        // ensure the clicked slot is gui (not player inv)
         if (slot < 0 || slot >= event.getInventory().getSize()) {
             return;
         }
 
-        System.out.println("click within display gui");
-
-        int column = slot % 9; // Calculate the column (zero-based)
-        int row = (slot - column) / 9; // Calculate the row (zero-based)
+        int column = slot % 9; // zero-based
+        int row = (slot - column) / 9;
 
         Vector2i selectedSlot = new Vector2i(column, row);
 
-        // Determine the multiplier based on shift-click & right click
-        // Regular click: 1
-        // Shift click: 10
-        // Right click OR shift right click: 0.1
+        // Regular click: 1x
+        // Shift click: 10x
+        // Right click OR shift right click: 0.1x
         double multiplier = event.isShiftClick() ? multiplierFastValue : 1.0;
         multiplier = multiplier / (event.isRightClick() ? (multiplierFastValue * multiplier) : 1.0);
 
         HashMap<String, String> commandMap = getCommandMap(multiplier, positionScale, rotationScale, sizeScale, brightnessScale);
 
         String command = commandMap.getOrDefault(selectedSlot.x + "," + selectedSlot.y, null);
-        System.out.println("clicked button command:");
-        System.out.println(command);
+        //System.out.println("clicked button command:");
+        //System.out.println(command);
         if (command == null) {return;}
 
-        // Execute the command
+        // todo: call functions directly instead of using tape I mean commands-
         player.performCommand(command);
     }
 
     @EventHandler
     public void onPlayerRotate(PlayerMoveEvent event) {
+        // todo: toggle this with a perm or command? performance go brrrrrrr
         // todo: if player is sneaking, multiply any movement by 0.1
 
         Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.LEAD || player.getInventory().getItemInMainHand().getType() == Material.SPECTRAL_ARROW || player.getInventory().getItemInMainHand().getType() == Material.LEAD) {
-            float fromYaw = event.getFrom().getYaw();
-            float toYaw = event.getTo().getYaw();
-            float fromPitch = event.getFrom().getPitch();
-            float toPitch = event.getTo().getPitch();
-            if (fromYaw != toYaw || fromPitch != toPitch) {
-                // The player has changed their rotation while holding a honey bottle.
-                float deltaYaw = toYaw - fromYaw;
-                float deltaPitch = toPitch - fromPitch;
-                // Get the player's selected VivDisplay
-                VivDisplay selectedVivDisplay = DisplayHandler.getSelectedVivDisplay(player);
 
-                // If the player has not selected a VivDisplay, send an error message and return
-                if (selectedVivDisplay == null) {
-                    player.sendMessage(errMap.get("noSelectedDisplay"));
-                    return;
-                }
-                if (selectedVivDisplay.isThisParent()) {
-                    if (player.getInventory().getItemInMainHand().getType() == Material.LEAD){
-                        DisplayGroupHandler.rotateHierarchy(selectedVivDisplay, new Vector3d(0, 0, -deltaYaw));
-                    }
-                    if (player.getInventory().getItemInMainHand().getType() == Material.SPECTRAL_ARROW){
-                        DisplayGroupHandler.rotateHierarchy(selectedVivDisplay, new Vector3d(deltaYaw, deltaPitch, 0));
-                    }
-                }else{
-                    if (player.getInventory().getItemInMainHand().getType() == Material.LEAD){
-                        selectedVivDisplay.changeRotation(0, 0, -deltaYaw, null);
-                    }
-                    if (player.getInventory().getItemInMainHand().getType() == Material.SPECTRAL_ARROW){
-                        selectedVivDisplay.changeRotation(deltaYaw, deltaPitch, 0, null);
-                    }
-                }
+        Material heldItemMaterial = player.getInventory().getItemInMainHand().getType();
+        if (!(heldItemMaterial == Material.LEAD || heldItemMaterial == Material.SPECTRAL_ARROW)) return;
+
+        float deltaYaw = event.getTo().getYaw() - event.getFrom().getYaw();
+        float deltaPitch = event.getTo().getPitch() - event.getFrom().getPitch();
+
+        // ensure selected display
+        VivDisplay selectedVivDisplay = DisplayHandler.getSelectedVivDisplay(player);
+        if (selectedVivDisplay == null) {
+            player.sendMessage(errMap.get("noSelectedDisplay"));
+            return;
+        }
+
+        if (selectedVivDisplay.isThisParent()) {
+            if (heldItemMaterial == Material.LEAD){
+                DisplayGroupHandler.rotateHierarchy(selectedVivDisplay, new Vector3d(0, 0, -deltaYaw));
+            }
+            if (heldItemMaterial == Material.SPECTRAL_ARROW){
+                DisplayGroupHandler.rotateHierarchy(selectedVivDisplay, new Vector3d(deltaYaw, deltaPitch, 0));
+            }
+        }else{
+            if (heldItemMaterial == Material.LEAD){
+                selectedVivDisplay.changeRotation(0, 0, -deltaYaw, null);
+            }
+            if (heldItemMaterial == Material.SPECTRAL_ARROW){
+                selectedVivDisplay.changeRotation(deltaYaw, deltaPitch, 0, null);
             }
         }
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        // todo: toggle this with a perm or command? performance go brrrrrrr
         Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.LEAD || player.getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD) {
-            Vector3d from = new Vector3d(event.getFrom().getX(), event.getFrom().getY(), event.getFrom().getZ());
-            Vector3d to = new Vector3d(event.getTo().getX(), event.getTo().getY(), event.getTo().getZ());
-            Vector3d delta = new Vector3d(to.x - from.x, to.y - from.y, to.z - from.z);
 
-            // Get the player's selected VivDisplay
-            VivDisplay selectedVivDisplay = DisplayHandler.getSelectedVivDisplay(player);
+        Material heldItemMaterial = player.getInventory().getItemInMainHand().getType();
+        if (!(heldItemMaterial == Material.LEAD || heldItemMaterial == Material.SPECTRAL_ARROW)) return;
 
-            // If the player has not selected a VivDisplay, send an error message and return
-            if (selectedVivDisplay == null) {
-                player.sendMessage(errMap.get("noSelectedDisplay"));
-                return;
+        Vector3d from = new Vector3d(event.getFrom().getX(), event.getFrom().getY(), event.getFrom().getZ());
+        Vector3d to = new Vector3d(event.getTo().getX(), event.getTo().getY(), event.getTo().getZ());
+        Vector3d delta = new Vector3d(to.x - from.x, to.y - from.y, to.z - from.z); // can't subtract Vector3d's lmao
+
+        // ensure selectedVivDisplay
+        VivDisplay selectedVivDisplay = DisplayHandler.getSelectedVivDisplay(player);
+        if (selectedVivDisplay == null) {
+            player.sendMessage(errMap.get("noSelectedDisplay"));
+            return;
+        }
+
+        if (selectedVivDisplay.isThisParent()) {
+            if (heldItemMaterial == Material.LEAD) {
+                DisplayGroupHandler.translateHierarchy(selectedVivDisplay, delta);
             }
-            if (selectedVivDisplay.isThisParent()) {
-                if (player.getInventory().getItemInMainHand().getType() == Material.LEAD) {
-                    DisplayGroupHandler.translateHierarchy(selectedVivDisplay, delta);
-                }
-                if (player.getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD) {
-                    DisplayGroupHandler.resizeHierarchy(selectedVivDisplay, (float) ((delta.x+delta.y+delta.z)*0.1+1));
-                }
-
-            }else{
-                if (player.getInventory().getItemInMainHand().getType() == Material.LEAD) {
-                    selectedVivDisplay.changePosition(delta);
-                }
-                if (player.getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD) {
-                    selectedVivDisplay.changeScale((delta.x+delta.y+delta.z)*0.1, null);
-                }
+            if (heldItemMaterial == Material.SPECTRAL_ARROW) {
+                DisplayGroupHandler.resizeHierarchy(selectedVivDisplay, (float) ((delta.x+delta.y+delta.z)*0.1+1));
             }
 
-
+        }else{
+            if (heldItemMaterial == Material.LEAD) {
+                selectedVivDisplay.changePosition(delta);
+            }
+            if (heldItemMaterial == Material.SPECTRAL_ARROW) {
+                selectedVivDisplay.changeScale((delta.x+delta.y+delta.z)*0.1, null);
+            }
         }
     }
 
